30f1acb3259b0eb19c42de4107c4bd67c154a161
immacool <65336226+immacool@users.noreply.github.com>
2023-02-07 12:00:45 +0000

    numbers вынесены за пределы функции так как являются неизменяемыми, удален аргумент cell у markCell, теперь функция возвращает HTMLDivElement

--- js/components.js
+++ js/components.js
@@ -1,13 +1,13 @@
+const numbers = [...Array(11).keys()];
+
+
 /**
  * Функция нумерации клетки 
- * @param {object} cell Клетка, которую необходимо пометить
  * @param {markerType} node Тип помечаемого символа (number или letter)
  * @param {number} index Индекс символа (от A до J, или от 1 до 10)
  */
-
-function markCell(cell, markerType, index) {
-  const numbers = [...Array(11).keys()];
-  const markText = document.createTextNode(
+function markCell(markerType, index) {
+  const textNode = document.createTextNode(
     (markerType == "number"
       ? numbers
       : numbers.map((_, i) => String.fromCharCode("A".charCodeAt(0) + i)))[
@@ -15,18 +15,16 @@ function markCell(cell, markerType, index) {
     ]
   );
 
-  const mark = document.createElement("div");
+  const markWraper = document.createElement("div");
+  markWraper.classList.add("marker");
+  markWraper.appendChild(textNode);
 
-  mark.classList.add("marker");
-  mark.appendChild(markText);
-
-  cell.appendChild(mark);
+  return markWraper;
 }
 
 /**
  * Функция, генерирующая игровые поля
  */
-
 function createFields() {
   const fieldSpace = document.getElementById("field-space");
 
@@ -39,17 +37,18 @@ function createFields() {
 
     for (let x = 0; x < 11; x++) {
       for (let y = 0; y < 11; y++) {
-        const cell = document.createElement("div");
+        const currentCell = document.createElement("div");
 
         if ((!x && y) || (x && !y)) {
-          const markerType = !x ? "number" : "letter";
-          markCell(cell, markerType, markerType == "number" ? y : x - 1);
+          const markerType = x == 0 ? "number" : "letter";
+          const node = markCell(markerType, markerType == "number" ? y : x - 1);
+          currentCell.appendChild(node);
         } else if (x && y) {
-          cell.id = `${fieldType}-field_${x}_${y}`;
-          cell.classList.add("game-cell", `${fieldType}-cell`);
+          currentCell.id = `${fieldType}-field_${x}_${y}`;
+          currentCell.classList.add("game-cell", `${fieldType}-cell`);
         }
 
-        field.appendChild(cell);
+        field.appendChild(currentCell);
       }
     }
   }
